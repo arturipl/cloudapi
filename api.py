4ea2c1cc4488dcea1ce9c59d863c088c0bfe3acb
arturipl <2180247@my.ipleiria.pt>
2019-03-15 00:56:43 +0000

    Adds new methods and comply results with teacher's

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import pymysql
 import pymysql.cursors
 import json
 from flask import Flask
+from flask import request
 
 api = Flask(__name__)
 
@@ -81,44 +82,150 @@ def voter_byname(name: str):
     query = "SELECT * FROM voters WHERE name LIKE %(voter name)s"
     params = {'voter name': name}
 
+    dataset = {'meta': {'handled_id': request.host}}
+
     conn = _api_getmysqlconn()
     with conn.cursor() as cursor:
-        cursor.execute(query, params)
-        read_data = cursor.fetchall()
+        records = cursor.execute(query, params)
+        dataset['meta']['records_on_data'] = records
+
+        if records:
+            dataset['result'] = cursor.fetchall()
+        else:
+            dataset['msg'] = 'not found'
 
     conn.close()
 
-    return json.dumps(read_data, separators=(',', ':'))
+    return json.dumps(dataset, separators=(',', ':'))
+
+
+@api.route(path + "/voter/bysection/<int:section>/<int:limit>", methods=['GET'])
+def voter_bysection(section: int, limit: int):
+    query = "SELECT * FROM voters WHERE section = %(section)s LIMIT %(limit)s"
+    params = {'section': section, 'limit': limit}
+
+    dataset = {'meta': {'handled_id': request.host}}
+
+    conn = _api_getmysqlconn()
+    with conn.cursor() as cursor:
+        records = cursor.execute(query, params)
+        dataset['meta']['records_on_data'] = records
+
+        if records:
+            dataset['result'] = cursor.fetchall()[0]
+        else:
+            dataset['msg'] = 'not found'
+
+    conn.close()
+
+    return json.dumps(dataset, separators=(',', ':'))
+
+
+@api.route(path + "/sections", methods=['GET'])
+def voter_sections():
+    query = "SELECT count(id) AS `total`, section FROM voters GROUP BY section"
+
+    dataset = {'meta': {'handled_id': request.host}}
+
+    conn = _api_getmysqlconn()
+    with conn.cursor() as cursor:
+        records = cursor.execute(query)
+        dataset['meta']['records_on_data'] = records
+        dataset['result'] = cursor.fetchall()
+
+    conn.close()
+
+    return json.dumps(dataset, separators=(',', ':'))
+
+
+@api.route(path + "/voters/<int:voter_id>/vote", methods=['PATCH'])
+def voters_setvote(voter_id: int):
+    query_update = "UPDATE voters SET has_voted = 1 WHERE id = %(voter id)s AND has_voted = 0"
+    query_select = "SELECT * FROM voters WHERE id = %(voter id)s"
+
+    param = {'voter id': voter_id}
+
+    dataset = {'meta': {'handled_id': request.host}}
+
+    conn = _api_getmysqlconn()
+    with conn.cursor() as cursor:
+        records = cursor.execute(query_update, *[param])
+        dataset['meta']['records_on_data'] = records
+
+        if records:
+            cursor.execute(query_select, *[param])
+            dataset['result'] = cursor.fetchall()[0]
+        else:
+            dataset['msg'] = 'Voter has already voted'
+
+    conn.close()
+
+    if records:
+        return json.dumps(dataset, separators=(',', ':'))
+    else:
+        return json.dumps(dataset, separators=(',', ':')), 409
+
+
+@api.route(path + "/voters/reset", methods=['PATCH'])
+def voters_reset():
+    query_update = "UPDATE voters SET has_voted = 0"
+
+    dataset = {'meta': {'handled_id': request.host}}
+
+    conn = _api_getmysqlconn()
+    with conn.cursor() as cursor:
+        records = cursor.execute(query_update)
+        dataset['meta']['records_on_data'] = records
+
+    conn.close()
+
+    return json.dumps(dataset, separators=(',', ':'))
 
 
 @api.route(path + "/voter/byvoter/<int:voterid>", methods=['GET'])
-def voter_byvoterid(voterid: int):
+def voter_byvoter(voterid: int):
     query = "SELECT * FROM voters WHERE voter_number = %(voter number)s"
+
     params = {'voter number': voterid}
+
+    dataset = {'meta': {'handled_id': request.host}}
 
     conn = _api_getmysqlconn()
     with conn.cursor() as cursor:
-        cursor.execute(query, params)
-        read_data = cursor.fetchall()
+        records = cursor.execute(query, params)
+        dataset['meta']['records_on_data'] = records
+
+        if records:
+            dataset['result'] = cursor.fetchall()[0]
+        else:
+            dataset['msg'] = 'not found'
 
     conn.close()
 
-    return json.dumps(read_data, separators=(',', ':'))
+    return json.dumps(dataset, separators=(',', ':'))
 
 
 @api.route(path + "/voter/byid/<int:uid>", methods=['GET'])
 def voter_byid(uid: int):
     query = "SELECT * FROM voters WHERE id = %(id)s"
+
     params = {'id': uid}
+
+    dataset = {'meta': {'handled_id': request.host}}
 
     conn = _api_getmysqlconn()
     with conn.cursor() as cursor:
-        cursor.execute(query, params)
-        read_data = cursor.fetchall()
+        records = cursor.execute(query, params)
+        dataset['meta']['records_on_data'] = records
+
+        if records:
+            dataset['result'] = cursor.fetchall()[0]
+        else:
+            dataset['msg'] = 'not found'
 
     conn.close()
 
-    return json.dumps(read_data, separators=(',', ':'))
+    return json.dumps(dataset, separators=(',', ':'))
 
 
 if __name__ == "__main__":
